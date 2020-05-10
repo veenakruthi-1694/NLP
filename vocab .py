e6ae29b89156fbfbdc58f63e821f559dbe41d659
veenakruthi-1694 <62419866+veenakruthi-1694@users.noreply.github.com>
2020-05-10 13:57:12 +0530

    Vocab

---
+++ vocab .py
@@ -0,0 +1,8 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("I love coffee")
+for word in doc:
+    lexeme = doc.vocab[word.text]
+    print(lexeme.text, lexeme.orth, lexeme.shape_, lexeme.prefix_, lexeme.suffix_,
+            lexeme.is_alpha, lexeme.is_digit, lexeme.is_title, lexeme.lang_)
